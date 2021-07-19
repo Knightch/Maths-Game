59c11e485d2554102f719352d706b9d026b6a6a0
knightCh <amankumarpandey2397@gmail.com>
2021-07-19 20:43:00 +0530

    Javascript - 2

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 var playing = false;
 var score;
+var action;
 
 document.getElementById("startreset").onclick = function () {
     if (playing == true) {
@@ -9,6 +10,39 @@ document.getElementById("startreset").onclick = function () {
         score = 0;
         document.getElementById("scoreValue").innerHTML = score;
         document.getElementById("startreset").innerHTML = "Reset Game";
-        document.getElementById("timeremaining").style.display = "block";
+        show("timeremaining");
+        timeremaining = 60;
+        startCountdown();
+        hide("gameOver");
     }
+}
+
+
+function startCountdown() {
+    action = setInterval(
+        function () {
+            timeremaining -= 1;
+            document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+            if (timeremaining == 0) {
+                stopCountdown();
+                show("gameOver");
+                document.getElementById("gameOver").innerHTML = "<p>game over!</p><p>your score is " + score + "</p>"
+                hide("timeremaining");
+                hide("wrong");
+                hide("correct");
+                playing = false;
+                document.getElementById("startreset").innerHTML = "Start Game";
+            }
+        }, 1000);
+
+}
+
+function stopCountdown() {
+    clearInterval(action);
+}
+function show(id) {
+    document.getElementById(id).style.display = "block";
+}
+function hide(id) {
+    document.getElementById(id).style.display = "none";
 }
